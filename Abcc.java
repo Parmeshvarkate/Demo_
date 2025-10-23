a47c8258c64ee66bd5a64fd65113dc4a7a5514d2
Parmeshvarkate <parmeshvarKate12@gmail.com>
2025-10-24 00:07:52 +0530

    2nd commit

--- Abcc.java
+++ Abcc.java
@@ -2,6 +2,7 @@ class Abcc{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
      System.out.println("Hello, java!");
+      System.out.println(" All is well !");
 
     }
 }
